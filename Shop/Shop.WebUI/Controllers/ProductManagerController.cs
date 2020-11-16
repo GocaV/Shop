daab4c079920d21d6f455ce0da0acad9190d19c4
GocaV <gorica.vasilevska@gmail.com>
2020-11-16 13:56:18 +0100

    Added Product Upload

--- Shop/Shop.WebUI/Controllers/ProductManagerController.cs
+++ Shop/Shop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,11 +34,17 @@ namespace Shop.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product) { 
+        public ActionResult Create(Product product, HttpPostedFileBase file) { 
         if(!ModelState.IsValid) {
                 return View(product);
             }
         else {
+
+                if (file != null){
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
+
                 context.Insert(product);
                 context.Commit();
 
@@ -60,7 +67,7 @@ namespace Shop.WebUI.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id) {
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file) {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
             {
@@ -71,9 +78,14 @@ namespace Shop.WebUI.Controllers
                 if(!ModelState.IsValid) {
                     return View(product);
                 }
+
+                if (file != null) {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                } 
+
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
 
